88a7b13e4c1d83fa750be147d8c2a7df88b0283d
Andrew Casey <accasey@hotmail.com.au>
2020-06-25 20:12:32 +0100

    Finished chapter 3

--- src/hypermodern_python/wikipedia.py
+++ src/hypermodern_python/wikipedia.py
@@ -9,11 +9,10 @@ API_URL = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
 def random_page(language="en"):
     try:
         url = API_URL.format(language=language)
-        
+
         with requests.get(url) as response:
             response.raise_for_status()
             return response.json()
     except requests.RequestException as error:
         message = str(error)
         raise click.ClickException(message)
-
--- tests/test_console.py
+++ tests/test_console.py
@@ -46,7 +46,7 @@ def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
     runner.invoke(console.main, ["--language=pl"])
     mock_wikipedia_random_page.assert_called_with(language="pl")
 
-    
+
 def test_main_fails_on_request_error(runner, mock_requests_get):
     mock_requests_get.side_effect = Exception("Boom")
     result = runner.invoke(console.main)
@@ -57,10 +57,3 @@ def test_main_prints_message_on_request_error(runner, mock_requests_get):
     mock_requests_get.side_effect = requests.RequestException
     result = runner.invoke(console.main)
     assert "Error" in result.output
-
-
-
-
-
-
-
