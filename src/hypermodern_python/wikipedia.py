fc0eaa1e8f31949c5bcce70280d79f463e0e9754
Andrew Casey <accasey@hotmail.com.au>
2020-06-25 13:17:04 +0100

    Finished chapter 2

---
+++ src/hypermodern_python/wikipedia.py
@@ -0,0 +1,19 @@
+import click
+import requests
+
+
+API_URL = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
+API_URL = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
+
+
+def random_page(language="en"):
+    try:
+        url = API_URL.format(language=language)
+        
+        with requests.get(url) as response:
+            response.raise_for_status()
+            return response.json()
+    except requests.RequestException as error:
+        message = str(error)
+        raise click.ClickException(message)
+
